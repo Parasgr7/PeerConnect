b529f84f1162888a4ed43d45240fd0b197bc3c67
Paras <parasgr484@gmail.com>
2017-05-23 22:59:56 +0530

    new

--- server.js
+++ server.js
@@ -63,4 +63,4 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`Server running on localhost:${port}`));
+server.listen(process.env.PORT || 3000, () => console.log(`Server running on localhost`, http.address().port));
